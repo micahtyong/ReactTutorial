8decb7f92f0279a115488922f264494ca1957c39
Micah Yong <micahtyong@berkeley.edu>
2019-05-21 15:39:32 -0700

    up to SVG Animations

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className = "Header">
+    <div className = "HeaderGroup">
+      <img src = {require('../images/Icon.png')} 
+      width = "30"></img>
+      <Link to = "/work">Work</Link>
+      <Link to = "/about">About</Link>
+      {/* <Link to = "/about"><button>About</button></Link> */}
     </div>
   </div>
 )
